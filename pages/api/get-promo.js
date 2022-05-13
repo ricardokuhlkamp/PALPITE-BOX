fa31676ac9d7a910a601d74c67d521ffdad8e809
Ricardo Kühlkamp da Silva <ric.kds@hotmail.com>
2022-05-12 21:48:00 -0300

    fix.

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -3,8 +3,13 @@ import { GoogleSpreadsheet } from 'google-spreadsheet'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-export default async(req, res) => {
+const fromBase64 = value => {
+    const buff = Buffer.from(value, 'base64');
+    return buff.toString('ascii');
+}
 
+export default async(req, res) => {
+    console.log(fromBase64(process.env.SHEET_PRIVATE_KEY))
     try{
         //await doc.useServiceAccountAuth(credentials)
         await doc.useServiceAccountAuth({
